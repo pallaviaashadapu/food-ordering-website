c2d8be1114f88836de42077208b93060f5430455
pallavi <168515560+pallaviaashadapu@users.noreply.github.com>
2024-06-25 21:37:24 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,228 @@
+
+const foodItem= [
+    
+{
+    id: 1,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.9,
+    price: 499,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+
+{
+    id: 2,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.5,
+    price: 365,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.4,
+    price: 698,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+
+
+
+
+{
+    id: 4,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.5,
+    price: 459,
+    img: 'images/chicken/Chicken-Curry.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.9,
+    price: 320,
+    img: 'images/chicken/Chicken-do-Pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.5,
+    price: 299,
+    img: 'images/chicken/Chicken-Masala.jpeg',
+    quantity: 1
+},
+
+
+
+{
+    id: 7,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.0,
+    price: 379,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 279,
+    img: 'images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+
+
+{
+    id: 9,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.1,
+    price: 330,
+    img: 'images/paneer/paneer-lababdar.jpg',
+    quantity: 1
+},
+
+
+
+{
+    id: 10,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.9,
+    price: 389,
+    img: 'images/vegetable/VEG-JALFREZI.jpg',
+    quantity: 1
+},
+
+{
+    id: 11,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.0,
+    price: 269,
+    img: 'images/vegetable/vegetable-curry.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.0,
+    price: 410,
+    img: 'images/vegetable/vegetable-kolhapuri.jpg',
+    quantity: 1
+},
+
+{
+    id: 13,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 345,
+    img: 'images/vegetable/vegetable-pakora.jpg',
+    quantity: 1
+},
+
+{
+    id: 14,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.4,
+    price: 170,
+    img: 'images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 15,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.6,
+    price: 194,
+    img: 'images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 190,
+    img: 'images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+
+{
+    id: 17,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.2,
+    price: 189,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+
+{
+    id: 18,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.4,
+    price: 150,
+    img: 'images/south indian/onion-uttapam.jpg',
+    quantity: 1
+},
+
+{
+    id: 19,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.1,
+    price: 200,
+    img: 'images/south indian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+//assigmnet
+{
+    id: 19,
+    name:'Gulab Jamun',
+    category:'Deserts',
+    rating:4.2,
+    price:250,
+    img:'images/desert/gulabjamun.jpg',
+    quantity:1,
+},
+{
+    id: 20,
+    name:'Kaju Katli',
+    category:'Deserts',
+    rating:4.3,
+    price:350,
+    img:'images/desert/kajukatli.jpg',
+    quantity:1,
+},
+{
+    id: 21,
+    name:'Tea',
+    category:'Hot Beverage',
+    rating:4.3,
+    price:50,
+    img:'images/beverage/tea.jpg',
+    quantity:1,
+},
+ 
+]
+
+export {foodItem};
